67268547587100e2dc80cf811a77860fe71a758f
ercas <edgarrimandocastro@gmail.com>
2018-03-27 13:48:50 -0400

    fixed docs(?)

--- docs-src/conf.py
+++ docs-src/conf.py
@@ -39,7 +39,9 @@ release = ''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc', 'sphinx.ext.viewcode'
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages'
 ]
 autoclass_content = "both"
 
